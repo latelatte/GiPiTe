0381ecd2340b7c88fc3e8748a50e20d2b60df8f9
Seiya-Ikeda <si721@york.ac.uk>
2024-05-17 22:58:55 +0100

    Initial Commit

---
+++ GiPiTe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GiPiTe
+//
+//  Created by Seiya Ikeda on 2024/05/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
